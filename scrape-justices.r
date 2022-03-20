29c9492f035f3c397f3a6524892b7a869f831840
sebmonte <smontesinos21@amherst.edu>
2022-03-20 10:10:12 -0400

    Working on problem #2

--- scrape-justices.r
+++ scrape-justices.r
@@ -1,9 +1,15 @@
+library(tidyverse)
+library(kableExtra)
+library(robotstxt) 
+library(rvest) 
+library(purrr) 
+
 
 url <- "https://en.wikipedia.org/wiki/List_of_justices_of_the_Supreme_Court_of_the_United_States"
 
 justice_table <- url %>%
   read_html() %>%
-  html_element("#content > #bodyContent >#mw-content-text > #mw-parser-output > table") %>%
+  html_elements("div#content > div#bodyContent > div#mw-content-text > div#mw-parser-output > table") %>%
   html_table() %>% 
   # Clean up variable names
   janitor::clean_names()
